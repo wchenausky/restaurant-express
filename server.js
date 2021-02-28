5539f22e941c41a51b651229fed8e30db88201bf
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-02-28 12:54:55 -0500

    server js edit

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const http = require('http');
-const fs = require('fs');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -9,57 +7,39 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const displayPage = (res, filename) => {
-    fs.readFile(`${__dirname}/${filename}`, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/html' });
-            res.end(
-              '<html><head><title>Oops</title></head><body><h1>Oops, there was a server-related error</h1></html>'
-            );
-          }
-        else {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.end(data);
-        }
-    }); 
-};
+let tables = [];
+let waitlist = [];
 
-const handleRequest = (request, response) => {
-    const path = req.url;
-    switch (path) {
-        case '/':
-            return displayPage(res, `home.html`);
-        case '/':
-            return displayPage(res, `home.html`);
-        case '/tables':
-            return displayPages(res, `tables.html`);
-        case '/reserve':
-            return displayPages(res, `reserve.html`);
-        default:
-            return displayPages(res, `404.html`);
-    }
-};
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+})
 
 app.get('/tables', function(req, res) {
-    return displayPage(res, `tables.html`);
-  });
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
 
-  app.get('/reserve', function(req, res) {
-    return displayPage(res, `reserve.html`);
-  });
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
 
-const server = http.createServer(handleRequest);
+/////////////arrays/////////////////////////
+app.get('/api/tables', function(req, res) {
+    res.json(tables);
+})
 
-let reserve = [];
-let wait = [];
+app.get('/api/waitlist', function(req, res) {
+    res.json(waitlist);
+})
 
-app.post('/reserve', (req, res) => {
-    const newRes = req.body;
-    reserve.push(newRes);
-    res.json(newRes);
-    console.log('new res:' + newRes);
+///////////////save info////////////////////
+app.post('/api/tables', function(req, res) {
+    if (tables.length < 5) {
+        tables.push(req.body);
+        res.send("tables");
+    } else {
+        waitlist.push(req.body)
+        res.send("waitlist");
+    }
 });
 
 app.listen(PORT, () => console.log(`Server is listening on PORT: ${PORT}`));
